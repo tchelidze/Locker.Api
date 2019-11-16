0040706ce7ae01e722a029df9a95505ce61f79e0
Bitchiko Tchelidze <bubachelidze1@gmail.com>
2019-11-16 21:21:07 +0400

    Added mongo db connection string to docker compose. added .dockerignore

--- src/Locker.Api/Features/AppInfo/AppInfoController.cs
+++ src/Locker.Api/Features/AppInfo/AppInfoController.cs
@@ -1,17 +1,17 @@
-using System.Reflection;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace Locker.Api.Features.AppInfo
 {
     public class AppInfoController : Controller
     {
-        private readonly string AssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        private readonly string _assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         [AllowAnonymous, HttpGet("api/app/info")]
         public object AppVersion() => new
         {
-            version = AssemblyVersion
+            version = _assemblyVersion
         };
-    };
+    }
 }
